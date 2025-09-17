a8a08e9f488696468cc56cb4de06113ff96a6416
Niviyaa21 <nivedhitharamalingam21@gmail.com>
2025-09-17 09:19:05 +0530

    first commit

---
+++ src/main/java/com/mystore/mystoreniv/MystorenivApplication.java
@@ -0,0 +1,13 @@
+package com.mystore.mystoreniv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MystorenivApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MystorenivApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mystore/mystoreniv/MystorenivApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mystore.mystoreniv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MystorenivApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
